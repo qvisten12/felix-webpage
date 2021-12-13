6dd384c5a2204a603668ae45f7334f34f23a1c71
Felix Ljungqvist <75997643+qvisten12@users.noreply.github.com>
2021-12-13 20:56:18 +0100

    implemented typescript

--- pages/api/spotify.ts
+++ pages/api/spotify.ts
@@ -36,7 +36,7 @@ export const getNowPlaying = async () => {
   });
 };
 
-const Index = async (_, res) => {
+const Index = async (_: any, res: any) => {
   const response = await getNowPlaying();
 
   if (response.status === 204 || response.status > 400) {
@@ -46,7 +46,9 @@ const Index = async (_, res) => {
   const song = await response.json();
   const isPlaying = song.is_playing;
   const title = song.item.name;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
+  const artist = song.item.artists
+    .map((_artist: any) => _artist.name)
+    .join(", ");
   const album = song.item.album.name;
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
