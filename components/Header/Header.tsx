f33d85bdda4c1bd89dc0a843140d56d91ef62392
Felix Ljungqvist <75997643+qvisten12@users.noreply.github.com>
2021-12-14 15:15:28 +0100

    updated typography

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,8 +7,10 @@ const Header = () => {
       mt-32 md:mt-56 container max-w-5xl mx-auto px-10 mb-10 md:mb-24"
     >
       <div className="flex flex-col flex-1 mb-10 md:mb-0 md:w-1/2">
-        <h1 className="text-5xl font-extrabold mb-5">Hi, I&apos;m Felix</h1>
-        <p className="font-medium text-2xl text-gray-500 mb-8 ">
+        <h1 className="text-5xl md:text-6xl font-extrabold mb-5">
+          Hi, I&apos;m Felix
+        </h1>
+        <p className="font-medium text-3xl text-gray-500 mb-8 ">
           a web developer
           <br />
           and tech enthusiast
@@ -25,7 +27,7 @@ const Header = () => {
       </div>
 
       <div className="flex flex-col max-w-lg md:w-1/2 text-gray-600 ">
-        <p className="font-medium text-lg mb-5">
+        <p className="font-medium text-xl mb-5">
           I’m a<span className="text-gray-700 font-bold"> Gothenburg</span>{" "}
           based aspiring Jr. front-end developer and designer. I&apos;m
           experienced in <span className="text-gray-700 font-bold">HTML5</span>,
@@ -35,7 +37,7 @@ const Header = () => {
           <span className="text-gray-700 font-bold">Next.js.</span>
         </p>
 
-        <p className="font-medium text-lg mb-5">
+        <p className="font-medium text-xl mb-5">
           I am currently studying full time at{" "}
           <span className="text-gray-700 font-bold">Borås University</span> in
           <span className="text-gray-700 font-bold"> Systems Science.</span>
